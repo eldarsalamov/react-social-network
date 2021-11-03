db6408f43058f26162628f1e724aeb9cf9c262b2
Eldar <eldar_salamov1999@mail.ru>
2021-11-03 11:44:04 +0300

    dialogs

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={s.item}>
+      <img src="https://cdn.icon-icons.com/icons2/1879/PNG/512/iconfinder-3-avatar-2754579_120516.png" />
+      {props.message}
+      <div>
+        <span>like</span> {props.likesCount}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
